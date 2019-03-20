17abb6fbb123a9ab07442625b0814751bcebcb08
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-03-20 14:08:08 +0700

    Upload the first project to Github

---
+++ Lab1/Bai1/Bai1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+
+namespace Bai1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Nhap ten mien: ");
+			string host = Console.ReadLine();
+			GetHostInfo(host);
+			Console.ReadKey();
+		}
+
+		private static void GetHostInfo(string host)
+		{
+			try
+			{
+				IPHostEntry hostEntry = Dns.GetHostEntry(host);
+				Console.WriteLine("Ten mien: " + hostEntry.HostName);
+				Console.WriteLine("Dia chi IP: ");
+				foreach (IPAddress item in hostEntry.AddressList)
+				{
+					Console.WriteLine(item.ToString() + " ");
+				}
+				Console.WriteLine();
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Khong phan giai duoc ten mien: " + host);
+				//throw;
+			}
+		}
+	}
+}

407439954ce2646895e6927620b2b23b89e01cfc
La Quốc Thắng <37357028+laquocthang@users.noreply.github.com>
2019-03-27 15:04:39 +0700

    Upload project

---
+++ Lab2/SimpleServerClient/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 5000);
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			Console.WriteLine("Connecting to server ... ");
+
+			try
+			{
+				Console.WriteLine("Connected Successfully");
+				serverSocket.Connect(serverEndPoint);
+			}
+			catch (SocketException se)
+			{
+				Console.WriteLine("Can't connect to server");
+				Console.ReadKey();
+				return;
+			}
+
+			while (true)
+			{
+				//Send message from client to server
+				string message = Console.ReadLine();
+				byte[] buff = Encoding.ASCII.GetBytes(message);
+				serverSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+				//Receive the message that server sent
+				buff = new byte[1024];
+				int bytes = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+				message = Encoding.ASCII.GetString(buff, 0, bytes);
+				Console.WriteLine("Sended Message: " + message);
+			}
+		}
+	}
+}
---
+++ Lab2/SimpleServerClient/Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 5000);
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			serverSocket.Bind(serverEndPoint);
+			serverSocket.Listen(10);
+
+			Socket clientSocket = serverSocket.Accept();
+
+			EndPoint clientEndPoint = clientSocket.RemoteEndPoint;
+			Console.WriteLine(clientEndPoint.ToString());
+
+			while (true)
+			{
+				//Receive the message from client
+				byte[] buff = new byte[1024];
+				int bytes = clientSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+				string message = Encoding.ASCII.GetString(buff, 0, bytes);
+				Console.WriteLine("Received Message: " + message);
+				//Send this message to client
+				clientSocket.Send(buff, 0, bytes, SocketFlags.None);
+			}
+		}
+	}
+}

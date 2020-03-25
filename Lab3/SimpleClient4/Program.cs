e2dae614ea1922cd5765fc3ac9dc3fbbef0d6cc8
La Quốc Thắng <quocthang0507@gmail.com>
2020-03-25 17:36:07 +0700

    Add new projects

---
+++ Lab3/SimpleClient4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClient4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			EndPoint remote = new IPEndPoint(IPAddress.Loopback, 5000); //Different from previous project: EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+			serverSocket.Connect(remote);
+			while (true)
+			{
+				Console.Write("> Input: ");
+				string message = Console.ReadLine();
+				byte[] buff = Encoding.UTF8.GetBytes(message);
+				serverSocket.SendTo(buff, 0, buff.Length, SocketFlags.None, serverEndPoint);
+
+				buff = new byte[1024];
+				int bytes = serverSocket.ReceiveFrom(buff, 0, buff.Length, SocketFlags.None, ref remote);
+				message = Encoding.UTF8.GetString(buff, 0, bytes);
+				Console.WriteLine("Server: " + message);
+			}
+		}
+	}
+}
---
+++ Lab3/SimpleServer4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleServer4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			byte[] buff = new byte[1024];
+			serverSocket.Bind(serverEndPoint);
+
+			Console.WriteLine("Waiting for client...");
+			EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+			serverSocket.ReceiveFrom(buff, ref remote);
+			Console.WriteLine("Client Info: " + remote.ToString());
+			Console.WriteLine("Client: " + Encoding.UTF8.GetString(buff).Replace("\0", ""));
+			while (true)
+			{
+				Console.Write("> Input: ");
+				string message = Console.ReadLine();
+				buff = Encoding.UTF8.GetBytes(message);
+				serverSocket.SendTo(buff, 0, buff.Length, SocketFlags.None, remote);
+
+				buff = new byte[1024];
+				int bytes = serverSocket.ReceiveFrom(buff, ref remote);
+				message = Encoding.UTF8.GetString(buff, 0, bytes);
+				Console.WriteLine("Client: " + message);
+			}
+		}
+	}
+}

2de26a6164454439c5fed5df3b09f839446bae83
La Quốc Thắng <quocthang0507@gmail.com>
2020-04-01 17:54:12 +0700

    Add new methods

--- Lab3.1/SimpleClient3/Program.cs
+++ Lab3.1/SimpleClient3/Program.cs
@@ -9,6 +9,11 @@ namespace SimpleClient
 	{
 		static void Main(string[] args)
 		{
+			RunWrong();
+		}
+
+		private static void RunOK()
+		{
 			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
 			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 			EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
@@ -25,5 +30,25 @@ namespace SimpleClient
 				Console.WriteLine("Server: " + message);
 			}
 		}
+
+		private static void RunWrong()
+		{
+			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+			while (true)
+			{
+				Console.Write("> Input: ");
+				string message = Console.ReadLine();
+				byte[] buff = Encoding.UTF8.GetBytes(message);
+				serverSocket.SendTo(buff, 0, buff.Length, SocketFlags.None, serverEndPoint);
+
+				// buff = new byte[1024];
+				int bytes = serverSocket.ReceiveFrom(buff, 0, buff.Length, SocketFlags.None, ref remote);
+				message = Encoding.UTF8.GetString(buff, 0, bytes);
+				Console.WriteLine("Server: " + message);
+			}
+		}
+
 	}
 }
--- Lab3.1/SimpleServer3/Program.cs
+++ Lab3.1/SimpleServer3/Program.cs
@@ -9,6 +9,11 @@ namespace SimpleServer
 	{
 		static void Main(string[] args)
 		{
+			RunWrong();
+		}
+
+		private static void RunOK()
+		{
 			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
 			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 			byte[] buff = new byte[1024];
@@ -32,5 +37,32 @@ namespace SimpleServer
 				Console.WriteLine("Client: " + message);
 			}
 		}
+
+		private static void RunWrong()
+		{
+			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			byte[] buff = new byte[1024];
+			string message;
+			int bytes;
+
+			serverSocket.Bind(serverEndPoint);
+
+			Console.WriteLine("Waiting for client...");
+			EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+			//int bytes = serverSocket.ReceiveFrom(buff, ref remote);
+			//Console.WriteLine("Client Info: " + remote.ToString());
+			//Console.WriteLine("Client: " + Encoding.UTF8.GetString(buff, 0, bytes));
+			while (true)
+			{
+				buff = new byte[1024];
+				bytes = serverSocket.ReceiveFrom(buff, 0, buff.Length, SocketFlags.None, ref remote);
+				message = Encoding.UTF8.GetString(buff, 0, bytes);
+				Console.WriteLine("Client: " + message);
+
+				serverSocket.SendTo(buff, 0, buff.Length, SocketFlags.None, remote);
+			}
+		}
+
 	}
 }

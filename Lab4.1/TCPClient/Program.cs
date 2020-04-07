22b185c6952a5eaa94437404b341c089eda2022e
La Quốc Thắng <quocthang0507@gmail.com>
2020-04-08 06:04:38 +0700

    Initialize projects via upload

---
+++ Lab4.1/TCPClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ Lab4.1/UDPClient2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UDPClient2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}

e243ffb6b21de3661ed01348481d8b36beb388d6
La Quốc Thắng <quocthang0507@gmail.com>
2020-04-08 06:41:10 +0700

    Update projects

--- Lab4.1/TCPClient/Program.cs
+++ Lab4.1/TCPClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TCPClient
 {
@@ -6,7 +8,40 @@ namespace TCPClient
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Connect("127.0.0.1", "La Quoc Thang");
+			Console.ReadKey();
+		}
+
+		static void Connect(string server, string message)
+		{
+			try
+			{
+				int port = 5000;
+				TcpClient client = new TcpClient(server, port); // Create a TcpClient
+
+				byte[] data = Encoding.UTF8.GetBytes(message); // Translate the passed message into UTF8 and store it as a byte array
+
+				NetworkStream stream = client.GetStream(); // Get a client stream for reading and writing
+
+				stream.Write(data, 0, data.Length); // Send the message to the connected TcpServer
+
+				data = new byte[1024]; // String to store the response UTF8 representation.
+				string response = String.Empty;
+				int bytes = stream.Read(data, 0, data.Length); // Read the first batch of the TcpServer response bytes.
+				response = Encoding.UTF8.GetString(data, 0, bytes);
+				Console.WriteLine("Server: " + response);
+
+				stream.Close();
+				client.Close();
+			}
+			catch (ArgumentNullException e)
+			{
+				Console.WriteLine("ArgumentNullException: {0}", e);
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine("SocketException: {0}", e);
+			}
 		}
 	}
 }
--- Lab4.1/TCPListener/Program.cs
+++ Lab4.1/TCPListener/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TCPListener
 {
@@ -6,7 +9,54 @@ namespace TCPListener
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// Listens for connections from TCP network clients
+			TcpListener server = null;
+			try
+			{
+				int port = 5000;    // Set the TCPListener on port 5000
+				IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+				server = new TcpListener(localAddr, port);
+				server.Start(); // Start listening for client requests
+
+				byte[] bytes = new byte[1024]; // Buffer for reading data
+				string data;
+
+				while (true)
+				{
+					Console.WriteLine("Waiting for a connection...");
+
+					TcpClient client = server.AcceptTcpClient(); // Perform a blocking call to accept requests
+					Console.WriteLine("Connected successfully");
+
+					data = null;
+					NetworkStream stream = client.GetStream(); // Get a stream object for reading and writing
+
+					int i;
+
+					while ((i = stream.Read(bytes, 0, bytes.Length)) != 0) //Loop to receive all the data sent by the client
+					{
+						data = Encoding.UTF8.GetString(bytes, 0, i); //Translate data bytes to a UTF8 string
+						Console.WriteLine("Client: " + data);
+
+						Console.Write("> Input: ");
+						data = Console.ReadLine();
+						byte[] msg = Encoding.UTF8.GetBytes(data);
+
+						stream.Write(msg, 0, msg.Length);
+					}
+					client.Close();
+				}
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine("SocketException: " + e);
+			}
+			finally
+			{
+				server.Stop(); // Stop listening for new clients
+			}
+			Console.ReadKey();
 		}
 	}
 }
